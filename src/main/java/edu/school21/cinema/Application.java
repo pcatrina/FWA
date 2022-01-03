7386ff0ccd98ccacd701ffd82affeee6f9e9cd89
Ruslan Khabibulin <habyab@mail.ru>
2022-01-03 12:45:05 +0500

    add spring-web depends

--- src/main/java/edu/school21/cinema/Application.java
+++
@@ -1,9 +0,0 @@
-package edu.school21.cinema;
-
-import edu.school21.cinema.config.CommonConfig;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-public class Application {
-    ApplicationContext applicationContext = new AnnotationConfigApplicationContext(CommonConfig.class);
-}
