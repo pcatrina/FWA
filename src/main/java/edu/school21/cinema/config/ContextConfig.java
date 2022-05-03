81adeffdc5a649412342a1ff750bf8c6934ff66a
Ruslan Khabibulin <pcatrina@student.21-school.ru>
2022-05-03 15:43:03 +0300

    tie Servlet Context with Spring Context

--- src/main/java/edu/school21/cinema/config/ContextConfig.java
+++ src/main/java/edu/school21/cinema/config/ContextConfig.java
@@ -1,9 +1,7 @@
 package edu.school21.cinema.config;
 
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
 @ComponentScan("edu.school21.cinema")
-public class CommonConfig {
+public class ContextConfig {
 }
--- src/main/java/edu/school21/cinema/listner/ApplicationListener.java
+++ src/main/java/edu/school21/cinema/listner/ApplicationListener.java
@@ -1,6 +1,6 @@
 package edu.school21.cinema.listner;
 
-import edu.school21.cinema.config.CommonConfig;
+import edu.school21.cinema.config.ContextConfig;
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
@@ -13,7 +13,7 @@ public class ApplicationListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         ServletContext servletContext = sce.getServletContext();
-        ApplicationContext springContext = new AnnotationConfigApplicationContext(CommonConfig.class);
+        ApplicationContext springContext = new AnnotationConfigApplicationContext(ContextConfig.class);
         servletContext.setAttribute("springContext", springContext);
     }
 }
