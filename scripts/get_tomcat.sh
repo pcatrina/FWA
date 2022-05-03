5623ad56ae79a268a88c4ec3b893d3b58e6af8be
Tenagrim <tenagrim@gmail.com>
2022-05-03 13:22:57 +0300

    add get_maven.sh

--- scripts/get_tomcat.sh
+++ scripts/get_tomcat.sh
@@ -2,7 +2,7 @@
 TOMCAT_LINK='https://dlcdn.apache.org/tomcat/tomcat-10/v10.0.20/bin/apache-tomcat-10.0.20.zip'
 TOMCAT_ZIP='apache-tomcat-10.0.20.zip'
 TOMCAT_UNPACKED='apache-tomcat-10.0.20'
-TOMCAT_FOLDER=../tomcat
+TOMCAT_FOLDER='../tools/tomcat'
 if [ ! -d $TOMCAT_FOLDER ] || [ ! -d $TOMCAT_FOLDER/$TOMCAT_UNPACKED ]; then
 	[ ! -d $TOMCAT_FOLDER ] && mkdir $TOMCAT_FOLDER
 	[ ! -f ./$TOMCAT_ZIP ] && curl -O --insecure $TOMCAT_LINK
