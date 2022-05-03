293f4d7ae71ce90b1359ee300b2ca543c5706e60
Ruslan Khabibulin <pcatrina@student.21-school.ru>
2022-05-03 17:35:35 +0300

    add schema.sql

--- src/main/java/edu/school21/cinema/models/User.java
+++ src/main/java/edu/school21/cinema/models/User.java
@@ -4,6 +4,7 @@ import lombok.Value;
 
 @Value
 public class User {
+    Long id;
     String firstName;
     String lastName;
     String phone;
---
+++ src/main/resources/sql/schema.sql
@@ -0,0 +1,9 @@
+CREATE SCHEMA IF NOT EXISTS fwa_schema;
+
+CREATE TABLE IF NOT EXISTS users (
+    user_id serial PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL ,
+    phone VARCHAR(10) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
