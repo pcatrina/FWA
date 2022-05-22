27815b4497c70237576e911f81aabbf12fbd9548
Ruslan Khabibulin <pcatrina@student.21-school.ru>
2022-05-22 16:25:08 +0300

    add getTime method

--- src/main/java/edu/school21/cinema/models/Log.java
+++ src/main/java/edu/school21/cinema/models/Log.java
@@ -5,6 +5,8 @@ import edu.school21.cinema.annotations.Id;
 import edu.school21.cinema.annotations.Table;
 import lombok.*;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Data
@@ -14,6 +16,10 @@ import java.util.Date;
 @Builder
 @AllArgsConstructor
 public class Log extends Entity{
+
+    private static String datePattern = "MM/dd/yyyy";
+    private static String timePattern = "\"h:mm a\"";
+
     @Id
     @Column(name = "object_id")
     Long objectId;
@@ -21,4 +27,14 @@ public class Log extends Entity{
     Long userId;
     Date date;
     String ip;
+
+    String getDataFormat(){
+        DateFormat df = new SimpleDateFormat(datePattern);
+        return df.format(date);
+    }
+
+    String getTimeFormat(){
+        DateFormat df = new SimpleDateFormat(timePattern);
+        return df.format(date);
+    }
 }
